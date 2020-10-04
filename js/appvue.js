c11d37b5fbc46ce01bdbfdcf9d518e062091a13c
giovanny2 <agiovannylutin@gmail.com>
2020-10-03 19:07:53 -0600

    mi primer aporte al grupo

---
+++ js/appvue.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Menu de retos de platzi segun cursos',
+      cursos:[
+          {id:1,curso:"Curso Definitivo de HTML y CSS",estado:1}
+        ]
+    }
+    })
