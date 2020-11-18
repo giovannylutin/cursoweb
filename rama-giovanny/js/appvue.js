fe49ff15d13baf60cf7232abfad23586a6c1a32e
giovanny2 <agiovannylutin@gmail.com>
2020-11-17 20:28:50 -0600

    se actualizo estructura para el siguiente curso

--- rama-giovanny/js/appvue.js
+++ rama-giovanny/js/appvue.js
@@ -16,15 +16,15 @@ var app = new Vue({
      methods:{
        obtenerCursos(){
         axios
-        .get('https://testgio90.000webhostapp.com/cursos')
-        // .get('http://192.168.1.17/api/cursos')
-        .then(response => (this.cursos = response.data))
+         .get('https://testgio90.000webhostapp.com/cursos'+'?nocache='+new Date().getTime())
+        //  .get('http://192.168.1.17/api/cursos')
+         .then(response => (this.cursos = response.data))
         //  console.log("hola")
        },
        obtenerClases(){
         axios
-        // .get('http://192.168.1.17/api/clases')
-         .get('https://testgio90.000webhostapp.com/clases')
+        //  .get('http://192.168.1.17/api/clases')
+         .get('https://testgio90.000webhostapp.com/clases'+'?nocache='+new Date().getTime())
         .then(response => (this.clases = response.data))
         //  console.log("hola")
        }
